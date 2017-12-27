64c0d77fe159212e8832c1ecbff1e09aa1621e70
Łukasz Topa <lukasztopa1993@gmail.com>
2017-12-27 17:34:42 +0100

    edit scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 $(function() {
   
   var url = 'https://restcountries.eu/rest/v1/name/';
+  
   var countriesList = $('#countries');
+  
+  $('search').click(searchCountries);
   
    function searchCountries() {
     var countryName = $('#country-name').val();
@@ -13,23 +16,11 @@ $(function() {
     });  
   }
   
-  
-  
-  $('search').click(searchCountries);
-  
-   function showCountriesList(resp) {
+  function showCountriesList(resp) {
       countriesList.empty();
        resp.forEach(function(item) {
          $('<li>').text(item.name).appendTo(countriesList);      
        });
     }
-  
-  searchCountries();
-  showCountriesList();
-  
- 
-  
-   
-  
   
 });
